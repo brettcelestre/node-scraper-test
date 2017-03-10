5f4d13db193fc4a25cd022444211279181d7c66c
Brett Celestre <bcelestre@gmail.com>
2017-03-10 10:53:16 -0800

    Updates osmosis test

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 let express = require('express')
 let app = express()
 let prettyjson = require('prettyjson')
+
 let osmosis = require('osmosis')
+
+let path = require('path')
+let childProcess = require('child_process')
+let phantomjs = require('phantomjs')
+let binPath = phantomjs.path
+
+let scraperjs = require('scraperjs');
+
+
 
 // console.log(prettyjson.render(test));
 
@@ -12,20 +22,36 @@ app.route('/data/:ticker')
   })
   .post(function (req, res) {
     console.log('/data/', req.params['ticker']);
-    
+
     let url = `http://performance.morningstar.com/fund/performance-return.action?t=${req.params['ticker']}&region=usa&culture=en-US`
 
-    osmosis
-      .get('www.brettcelestre.com')
-      .find('h1')
-      .set('name')
-      .data(function(data) {
-            // do something with data data
-            console.log('Data', data);
-        })
-      .log(console.log)
-      .error(console.log)
-      .debug(console.log)
+    // SCRAPERJS TEST
+    // scraperjs.StaticScraper.create(url)
+    //   .scrape(function($) {
+    //     return $("td.row_data").map(function() {
+    //       return $(this).text();
+    //     }).get();
+    //   })
+    //   .then(function(news) {
+    //       console.log(news);
+    //   })
+
+    // OSMOSIS TEST
+    // osmosis
+    //   .get(url)
+    //   // .get('http://www.yourhtmlsource.com/tables/basictables.html')
+    //   .find('h1')
+    //   .set('TITLE')
+    //   // https://css2xpath.github.io/
+    //   // div.wrapper div.r_bodywrap div.col5 div#total_returns_page div#tabday div#tab-1day-content table.print97 tbody tr.action td.row_data
+    //   .paginate('.//div[contains(concat(" ",normalize-space(@class)," ")," wrapper ")]//div[contains(concat(" ",normalize-space(@class)," ")," r_bodywrap ")]//div[contains(concat(" ",normalize-space(@class)," ")," col5 ")]//div[@id="total_returns_page"]//div[@id="tabday"]//div[@id="tab-1day-content"]//table//tr//td', 5)
+    //   .set('TABLE_DATA')
+    //   .data(function(data) {
+    //       console.log('Data', data);
+    //     })
+    //   .log(console.log)
+    //   .error(console.log)
+    //   .debug(console.log)
 
 
     // console.log(obj);
